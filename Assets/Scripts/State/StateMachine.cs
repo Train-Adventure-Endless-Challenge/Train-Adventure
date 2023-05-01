2376609ccceba1b54d6c7e247202c7c548e677eb
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-02 00:11:08 +0900

    Update StateMachine.cs
    변수 파라미터

--- Assets/Scripts/State/StateMachine.cs
+++ Assets/Scripts/State/StateMachine.cs
@@ -9,6 +9,9 @@ public class StateMachine<T>
     private State<T> _previousState;    // 이전 State
     private float _elapsedTimeInState;  // State 유효 시간 
     private Dictionary<System.Type, State<T>> _states = new Dictionary<System.Type, State<T>>();    //상태 모음
+    public State<T> CurrentState { get; }
+    public State<T> PreviousState { get; }
+    public float ElapsedTimeInState { get; }
 
     /// <summary>
     /// State dictionary에 초기화
