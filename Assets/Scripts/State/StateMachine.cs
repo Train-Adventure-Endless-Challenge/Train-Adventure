971e151ff21ede954d019e9ab3c3a4b62a7663ef
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-02 00:06:13 +0900

    Update StateMachine.cs
    State Set 함수들

--- Assets/Scripts/State/StateMachine.cs
+++ Assets/Scripts/State/StateMachine.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StateMachine<T> 
 {
+    private State<T> _currentState;     // 현재 State
+    private State<T> _previousState;    // 이전 State
+    private float _elapsedTimeInState;  // State 유효 시간 
+    private Dictionary<System.Type, State<T>> _states = new Dictionary<System.Type, State<T>>();    //상태 모음
 
-
-    // Start is called before the first frame update
-    void Start()
+    /// <summary>
+    /// State dictionary에 초기화
+    /// </summary>
+    /// <param name="state"></param>
+    public void AddState(State<T> state)
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update(float deltaTime)
     {
-        
+            
+    }
+
+    /// <summary>
+    /// 상태 변경 
+    /// </summary>
+    /// <param name="state">변경할 상태</param>
+    public void ChangeState(State<T> state)
+    {
+
     }
 }
