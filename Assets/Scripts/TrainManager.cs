16d85eea62545d5d7ff22b53037bbe33cdf79428
정명직 <jmj51755245@gmail.com>
2023-05-02 20:10:46 +0900

    Create TrainManager

---
+++ Assets/Scripts/TrainManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TrainManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] trainObjects;
+
+    private Vector3 trainPositionOffset;
+    private int currentTrain;
+
+    private void Start()
+    {
+        trainPositionOffset = trainObjects[1].transform.position - trainObjects[0].transform.position;
+        currentTrain = 0;
+    }
+
+    [ContextMenu("!")]
+    public void StartNextStage()
+    {
+        trainObjects[currentTrain].transform.position += (trainPositionOffset * 2);   
+        currentTrain ^= 1;
+        Debug.Log(currentTrain);
+    }
+
+}
