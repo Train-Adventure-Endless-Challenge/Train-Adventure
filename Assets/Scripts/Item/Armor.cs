0b182431bab30e085e3cbae82161d226de632cf3
EnSung <dmstjd0911@naver.com>
2023-05-04 13:49:35 +0900

    :sparkles: | Armor 클래스 제작
    - Item 클래스를 상속받음
    - 방어구의 종류만 멤버 변수로 가지고있음

---
+++ Assets/Scripts/Item/Armor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Armor : Item
+{
+    [SerializeField] protected ArmorType _armorType;            // 방어구 종류
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
