07ef6a037c3087530de243f5062746930556eb1a
EnSung <dmstjd0911@naver.com>
2023-05-04 12:27:23 +0900

    :sparkles: | Weapon 클래스 제작
    - Weapon은 Item 클래스를 상속 받음
    - Attack함수와 UseActiveSkill이 각각 공격과 스킬의 로직을 가지고있도록 설계함

---
+++ Assets/Scripts/Item/Weapon.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : Item
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+
+    /// <summary>
+    /// 공격 로직이 들어있는 함수
+    /// </summary>
+    public virtual void Attack()
+    {
+        
+    }
+
+    /// <summary>
+    /// 액티브 스킬 로직이 들어있는 함수
+    /// </summary>
+    public virtual void UseActiveSkill()
+    {
+
+    }
+
+}
