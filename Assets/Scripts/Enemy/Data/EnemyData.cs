17e344c7939edb41ddaa049666f0adabb3eb8947
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-12 11:28:50 +0900

    :bug: ㅣ enemyData 접근 제한

--- Assets/Scripts/Enemy/Data/EnemyData.cs
+++ Assets/Scripts/Enemy/Data/EnemyData.cs
@@ -4,26 +4,18 @@ using UnityEngine;
 
 public enum EnemyType
 {
-    range,      // ¿ø°Å¸®
-    melee       // ±Ù°Å¸®
+    range,      // ì›ê±°ë¦¬
+    melee       // ê·¼ê±°ë¦¬
 }
 
 [CreateAssetMenu(fileName = "Enemy Data", menuName = "Scriptable Object/Enemy Data", order = int.MaxValue - 9)]
 public class EnemyData : ScriptableObject
 {
-    private string _name;
-    private float _hp;
-    private float _damage;          
-    private float _moveSpeed;       
-    private float _attackSpeed;
-    private float _attackRange;
-    private EnemyType _enemyType;
-
-    public string Name { get { return _name; } }
-    public float HP { get { return _hp; } }
-    public float Damage { get { return _damage; } }
-    public float MoveSpeed { get { return _moveSpeed; } }
-    public float AttackSpeed { get { return _attackSpeed; } }
-    public float AttackRange { get { return _attackRange; } }
-    public EnemyType EnemyType { get { return EnemyType; } }
+    public string _name;
+    public float _hp;
+    public float _damage;          
+    public float _moveSpeed;       
+    public float _attackSpeed;
+    public float _attackRange;
+    public EnemyType _enemyType;
 }
