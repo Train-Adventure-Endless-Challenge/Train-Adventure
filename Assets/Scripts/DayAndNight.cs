dab9a69acf649f09c43f7ae47594241da854eeb3
정명직 <jmj51755245@gmail.com>
2023-05-12 11:17:58 +0900

    🤩 | 밤 낮 구현
    Directional Light을 회전 시켜 밤 낮 구현

---
+++ Assets/Scripts/DayAndNight.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    [SerializeField] private float secondPerRealTimeSecond; // 게임 세계에서의 100초 = 현실 세계의 1초
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // 계속 태양을 X 축 중심으로 회전. 현실시간 1초에  0.1f * secondPerRealTimeSecond 각도만큼 회전
+        transform.Rotate(Vector3.right, 0.1f * secondPerRealTimeSecond * Time.deltaTime);
+
+    }
+}
+
