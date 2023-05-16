6dfb852e41ddcb80559daf846b105658844956a5
EnSung <dmstjd0911@naver.com>
2023-05-16 23:12:34 +0900

    :pencil2: | 기어수 변수 이름 변경 gearCount to gearAmount
    - 기어의 수를 나타내는 변수에 count는 맞지 않다고 생각되어 amount로 변경함

--- Assets/Scripts/GearSystem/GearSystem.cs
+++ Assets/Scripts/GearSystem/GearSystem.cs
@@ -4,23 +4,24 @@ using UnityEngine;
 
 public class GearSystem : MonoBehaviour
 {
-    private int _gearCount;
-    public int GearCount { get { return _gearCount; } }
+    private int _gearAmount;
+    public int GearAmount { get { return _gearAmount; } }
 
     public void AddGear(int addCount)
     {
         if (addCount < 0) return;
 
-        _gearCount += addCount;
+        _gearAmount += addCount;
         // TODO: 추후 UI 변경 등 함수 실행
     }
 
     public void SubGear(int subCount)
     {
-        if (_gearCount < subCount || subCount > 0) return;
-        _gearCount -= subCount;
+        if (_gearAmount < subCount || subCount > 0) return;
+        _gearAmount -= subCount;
         // TODO: 추후 UI 변경 등 함수 실행
     }
 
 
 }
+            
