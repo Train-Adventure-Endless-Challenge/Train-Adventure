f7a0c7b6aa9b3ad6278f08c91fd7a77881fa8ac3
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-05-16 15:59:51 +0900

    :fire: ㅣ dengerLine Delete

--- Assets/Scripts/Enemy/Type/EnemyController_Range.cs
+++ Assets/Scripts/Enemy/Type/EnemyController_Range.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class EnemyController_Range : EnemyController
 {
-    [Header("Attack")]
-    public GameObject _dangerLinePrefab;
-
     protected override void Start()
     {
         base.Start();
@@ -19,10 +16,5 @@ public class EnemyController_Range : EnemyController
     protected override void Update()
     {
         base.Update();
-    }
-
-    public GameObject CloneDangerLinePrefab()
-    {
-        return Instantiate(_dangerLinePrefab,Vector3.zero, Quaternion.identity);
     }
 }
