c3d08dda46705e8adff799457023c27e0c5462b8
정명직 <jmj51755245@gmail.com>
2023-05-16 12:17:07 +0900

    🎨 | 변수명 변경

--- Assets/Scripts/DayAndNight/DayAndNight.cs
+++ Assets/Scripts/DayAndNight/DayAndNight.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class DayAndNight : MonoBehaviour
 {
-    [SerializeField] private Material[] sky;
+    [SerializeField] private Material[] _sky;
 
-    private float degree;
+    private float _degree;
 
     void Start()
     {
         StartCoroutine(ChangeSkyboxCor(0));
 
-        degree = 0;
+        _degree = 0;
     }
 
     void Update()
     {
-        degree += Time.deltaTime * 1.5f;
-        if (degree >= 360)
-            degree = 0;
+        _degree += Time.deltaTime * 1.5f;
+        if (_degree >= 360)
+            _degree = 0;
 
-        RenderSettings.skybox.SetFloat("_Rotation", degree);
+        RenderSettings.skybox.SetFloat("_Rotation", _degree);
     }
 
     public IEnumerator ChangeSkyboxCor(int index)
     {
         yield return StartCoroutine(FadeSkyboxCor(1, 0));
 
-        yield return RenderSettings.skybox = sky[index];
+        yield return RenderSettings.skybox = _sky[index];
 
         yield return StartCoroutine(FadeSkyboxCor(0, 1));
     }
