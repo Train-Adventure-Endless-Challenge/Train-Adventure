132dc6f99e3708ddf8554f01cdc0fe9279e7e170
이은우 <80678083+wjdaudwlr@users.noreply.github.com>
2023-05-28 17:16:26 +0900

    🤩 | 기차 프리팹 구현
    - 기차 내 오브젝트, 몬스터 배치

--- Assets/Scripts/Train/NomalTrain.cs
+++ Assets/Scripts/Train/NomalTrain.cs
@@ -1,35 +1,36 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class NomalTrain : Train
 {
-    [SerializeField] private EnemySpawnPoint[] enemySpawnPoint;
-    [SerializeField] Animation doorAnimation;
+    [SerializeField] private EnemySpawnPoint[] _enemySpawnPoints;
+    [SerializeField] Animation _frontDoorAnimation;
+    [SerializeField] Animation _backDoorAnimation;
 
-    private List<GameObject> currentTrainEnemys = new List<GameObject>();
+    private List<GameObject> _currentTrainEnemys = new List<GameObject>();
 
-    private bool isInitialize;
-    private bool isClear;
+    private bool _isInitialize;
+    private bool _isClear;
 
     public override void Init()
     {
-        for (int i = 0; i < enemySpawnPoint.Length; i++)
+        for (int i = 0; i < _enemySpawnPoints.Length; i++)
         {
-            currentTrainEnemys.Add(Instantiate(enemySpawnPoint[i].enemy, enemySpawnPoint[i].enemyTr.position, Quaternion.identity));
+            _currentTrainEnemys.Add(Instantiate(_enemySpawnPoints[i].enemy, _enemySpawnPoints[i].enemySpawmPointTr.position, Quaternion.identity));
             
             // 임시로 조정한 크기
-            currentTrainEnemys[i].transform.localScale = new Vector3(0.4f, 0.4f, 0.4f); 
+            _currentTrainEnemys[i].transform.localScale = new Vector3(0.4f, 0.4f, 0.4f); 
         }
 
-        isInitialize = true;
+        _isInitialize = true;
     }
 
     private void Update()
     {
-        if (currentTrainEnemys.All(i => i == null) && isInitialize && !isClear)
+        // 임시로 테스트 하기위한 코드 
+        if (_currentTrainEnemys.All(i => i == null) && _isInitialize && !_isClear)
         {
             ClearStage();
         }
@@ -37,8 +38,9 @@ public class NomalTrain : Train
 
     private void ClearStage()
     {
-        doorAnimation.Play();
-        isClear = true;
+        _frontDoorAnimation.Play();
+        //_frontDoorAnimation.clip = _frontDoorAnimation.GetClip("Door_Close");
+        _isClear = true;
     }
 }
 
@@ -46,6 +48,6 @@ public class NomalTrain : Train
 [Serializable]
 class EnemySpawnPoint
 {
-    public Transform enemyTr;
+    public Transform enemySpawmPointTr;
     public GameObject enemy;
 }
--- Assets/Scripts/Train/TrainManager.cs
+++ Assets/Scripts/Train/TrainManager.cs
@@ -4,23 +4,27 @@ using UnityEngine;
 
 public class TrainManager : SceneSingleton<TrainManager>
 {
-    [SerializeField] private GameObject[] _trainObjects;
     [SerializeField] private Transform _backgroundGroup;
 
-    [SerializeField] private Train _currentTrain;               // 현재 기차       
-    [SerializeField] private Train _nextTrain;                  // 다음 기차
+    [Header("Train")]
+    [SerializeField] private GameObject[] _nomalTrainObjects;
+    [SerializeField] private GameObject _bossTrainObject;
+    [SerializeField] private GameObject _storeTrainObject;
+
+    private Train _currentTrain;               // 현재 기차       
+    private Train _nextTrain;                  // 다음 기차
 
     private Vector3 _trainInterval = new Vector3(0, 0, 1.5f);   // 기차 간격
     private Vector3 _startPosition = Vector3.zero;           
     
     private void Start()
     {
-        _currentTrain = CreateTrain(0, _startPosition);
+        _currentTrain = CreateTrain(_nomalTrainObjects[Random.Range(0, _nomalTrainObjects.Length)], _startPosition);
         _currentTrain.Init();
 
-        _nextTrain = CreateTrain(0, _startPosition);
+        _nextTrain = CreateTrain(_nomalTrainObjects[Random.Range(0, _nomalTrainObjects.Length)], _startPosition);
 
-        _nextTrain.transform.position += new Vector3(0, 0,
+        _nextTrain.transform.position += new Vector3(0, 0, 
             (_nextTrain._floor.transform.localScale.z / 2)
             + (_currentTrain._floor.transform.localScale.z / 2)) + _trainInterval;
     }
@@ -28,17 +32,17 @@ public class TrainManager : SceneSingleton<TrainManager>
     [ContextMenu("!")] // 테스트 용
     public void RandomNextStage()
     {
-        StartNextTrain(Random.Range(0, 3));
+        StartNextTrain(_nomalTrainObjects[Random.Range(0,_nomalTrainObjects.Length)]);
     }
 
     // 다음 기차로 이동했을 때 함수
-    public void StartNextTrain(int trainIndex)
+    public void StartNextTrain(GameObject train)
     {
         _currentTrain.DestroyGameObejct();
         _currentTrain = _nextTrain;
         _currentTrain.Init();
 
-        _nextTrain = CreateTrain(trainIndex, _currentTrain.transform.position);
+        _nextTrain = CreateTrain(train, _currentTrain.transform.position);
 
         // 두 개의 기차 칸의 바닥 절반의 크기와 trainInterval 더해 nextPosition을 구함
         Vector3 nextPosition = new Vector3(0, 0, (_nextTrain._floor.transform.localScale.z / 2)
@@ -50,9 +54,9 @@ public class TrainManager : SceneSingleton<TrainManager>
     }
 
     // 기차 생성 함수
-    private Train CreateTrain(int trainIndex, Vector3 position)
+    private Train CreateTrain(GameObject train , Vector3 position)
     {
-        return Instantiate(_trainObjects[trainIndex], position, Quaternion.identity).GetComponent<Train>();
+        return Instantiate(train, position, Quaternion.identity).GetComponent<Train>();
     }
 
 }
