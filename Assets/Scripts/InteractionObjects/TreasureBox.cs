3cd4e5506512afe95051b267b6c3a34af4996e17
EnSung <dmstjd0911@naver.com>
2023-06-02 16:07:41 +0900

    :sparkles: | 보물상자 스크립트 추가
    - 로직은 아직 없음

---
+++ Assets/Scripts/InteractionObjects/TreasureBox.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureBox : InteractionObject
+{
+    public override void Interact()
+    {
+        //TODO: 애니메이션 실행
+        //TODO: 아이템 소환
+        //TODO: 아이템 나오게하기
+    }
+
+}
