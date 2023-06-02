f2e14febb554f65d87abd7d1d307c1c7dfab31cc
EnSung <dmstjd0911@naver.com>
2023-06-02 16:07:19 +0900

    :sparkles: | Gear 오브젝트 제작
    - 플레이어가 가까이오면 Gear오브젝트가 플레이어에게 다가감
    - Gear오브젝트가 Player에게 닿으면 Gear 수 증가
    - 모델 적용은 아직 안된 상태

---
+++ Assets/Scripts/InteractionObjects/Gear.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gear : MonoBehaviour
+{
+    private GearMove move;
+    private int _acquisitionsGear;
+    [SerializeField] private float _speed;
+    public int AcquisitionGear
+    {
+        get { return _acquisitionsGear;}
+        set { _acquisitionsGear = value; }
+    }
+
+    private void Start()
+    {
+        move = GetComponentInChildren<GearMove>();
+        move.Speed = _speed;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GearSystem.Instance.AddGear(_acquisitionsGear);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/InteractionObjects/GearMove.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearMove : MonoBehaviour
+{
+    public float Speed { get; set; }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("MOVE IN");
+        if (other.gameObject.CompareTag("Player"))
+        {
+            transform.parent.transform.position = Vector3.MoveTowards(transform.position, other.transform.position,Speed * Time.deltaTime);
+        }
+
+
+    }
+}
