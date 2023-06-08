7513c8de784beb0b1b4b4f27ca9ad740c6dfc1b2
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-08 11:15:41 +0900

    :sparkles: ㅣ Enenmy_Scientist Controller

---
+++ Assets/Scripts/Enemy/Type/EnemyContoller_Scientist.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyContoller_Scientist : EnemyController
+{
+    protected override void Start()
+    {
+        base.Start();
+
+        _stateMachine.AddState(new EnemyDieState());
+        _stateMachine.AddState(new EnemyMoveState());
+        _stateMachine.AddState(new EnemyAttackState());
+    }
+
+    /// <summary>
+    /// Skill 일지 일반 공격일지 체크하는 함수
+    /// </summary>
+    public void SkillCheck()
+    {
+
+    }
+
+
+}
