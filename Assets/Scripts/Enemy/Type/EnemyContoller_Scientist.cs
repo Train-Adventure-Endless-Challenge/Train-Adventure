330440384d4b3a840192790e6e383e6ddc0d0a37
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-09 09:16:47 +0900

    :sparkles: ㅣ Skill Attack Logic

--- Assets/Scripts/Enemy/Type/EnemyContoller_Scientist.cs
+++ Assets/Scripts/Enemy/Type/EnemyContoller_Scientist.cs
@@ -1,3 +1,4 @@
+using Cinemachine.Utility;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,13 @@ using UnityEngine;
 public class EnemyContoller_Scientist : EnemyController
 {
     [SerializeField] float _attackDelayTime = 10.0f;
-    float _timer = 0f;
+    float _skillTimer = 0f;
+    private bool _isSkill;      
 
     [Header("Attack")]
     public Transform _attackTransform;
     [SerializeField] GameObject _bulletPrefab;      // 일반 총알 프리팹
+    public Transform _bombTransform;
     [SerializeField] GameObject _bombPrefab;        // 폭탄 프리팹
 
 
@@ -22,13 +25,22 @@ public class EnemyContoller_Scientist : EnemyController
         _stateMachine.AddState(new EnemyAttackState());
     }
 
+    protected override void Update()
+    {
+        base.Update();
+
+        if (_isSkill)
+            _skillTimer += Time.deltaTime;
+
+    }
+
     /// <summary>
     /// Skill 일지 일반 공격일지 체크하는 함수
     /// </summary>
     public void SkillCheck()
     {
         _isCurrentAttack = true;
-        if(_timer <= _attackDelayTime)
+        if(_skillTimer >= _attackDelayTime)
         {
             StartCoroutine(AttackSkill());
         }
@@ -44,7 +56,43 @@ public class EnemyContoller_Scientist : EnemyController
     /// <returns></returns>
     IEnumerator AttackSkill()
     {
-        yield return null;
+        _isSkill = true;
+
+        // 잘못 인식된 경우 나가기
+        if (Vector3.Distance(transform.position, _player.transform.position) > AttackRange + _agent.stoppingDistance)
+        {
+            _isCurrentAttack = false;
+            yield break;
+        }
+
+        _isCurrentAttack = true;
+        _agent.isStopped = true;
+
+        Vector3 dir = _player.transform.position - transform.position;
+        float timer = 0;
+        while (timer <= 0.5f)       // 약간의 delay. temp 값.
+        {
+            timer += Time.deltaTime;
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 2);
+
+            yield return new WaitForEndOfFrame();
+
+        }
+
+        // 특수 공격 모션 필요
+
+        // 공격
+        GameObject bombObj = Instantiate(_bombPrefab, _bombTransform.position, Quaternion.identity);        // 폭탄 생성
+
+
+        yield return new WaitForSeconds(AttackSpeed);
+        _agent.isStopped = false;
+        _isCurrentAttack = false;
+
+        _isSkill = false;
+        _skillTimer = 0;
+
     }
 
     /// <summary>
@@ -89,7 +137,6 @@ public class EnemyContoller_Scientist : EnemyController
         yield return new WaitForSeconds(AttackSpeed);
         _agent.isStopped = false;
         _isCurrentAttack = false;
-        yield return null;
     }
 
     /// <summary>
