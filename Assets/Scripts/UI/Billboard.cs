c46776549b41c0622fc080bb56c17138804f5aa0
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-18 22:47:59 +0900

    :fire: ㅣ 주석 제거

--- Assets/Scripts/UI/Billboard.cs
+++ Assets/Scripts/UI/Billboard.cs
@@ -7,13 +7,6 @@ public class Billboard : MonoBehaviour
 
     VCamera _cam => VCamera.Instance;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.LookAt(transform.position + _cam.transform.rotation * Vector3.back,
