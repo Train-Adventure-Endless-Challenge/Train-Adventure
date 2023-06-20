cf4b064d22deb037183ef8d3546c5e71745f47ca
EnSung <dmstjd0911@naver.com>
2023-06-20 14:58:18 +0900

    :sparkles: | 기차 사운드 추가

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class SoundManager : GlobalSingleton<SoundManager>
 {
     private AudioSource _bgSoundSource;
+
+    [SerializeField] AudioClip test;
+
+    private void Start()
+    {
+        _bgSoundSource= GetComponent<AudioSource>();
+        BgSoundPlay(test);
+    }
     public void SFXPlay(string sfxName, AudioClip clip)
     {
         GameObject go = new GameObject(sfxName + "Sound");
