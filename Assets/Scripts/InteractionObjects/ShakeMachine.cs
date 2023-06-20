994c039410591702f700057e8636e8cc9e087359
EnSung <dmstjd0911@naver.com>
2023-06-20 10:39:25 +0900

    :sparkles: | ShakeMachine 모델 적용 및 애니메이션추가

--- Assets/Scripts/InteractionObjects/ShakeMachine.cs
+++ Assets/Scripts/InteractionObjects/ShakeMachine.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ShakeMachine : InteractionObject
 {
+    Animator _anim;
+
+    private void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
     public override void Interact()
     {
         int necessaryGear = (int)ShakeManager.Instance.ShakeAmount * 5;
@@ -11,6 +17,22 @@ public class ShakeMachine : InteractionObject
         {
             ShakeManager.Instance.ClearShake();
             GearSystem.Instance.SubGear(necessaryGear);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _anim.SetBool("Open", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _anim.SetBool("Open", false);
         }
     }
 }
