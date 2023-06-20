c2b5dc4e867075ec2ed3f7f1af8033a699b4d5b6
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-20 14:43:48 +0900

    🤩 l scientist enemy
    Scene 에서 테스트 못함

--- Assets/Scripts/Enemy/Type/EnemyController_Scientist.cs
+++ Assets/Scripts/Enemy/Type/EnemyController_Scientist.cs
@@ -12,7 +12,7 @@ public class EnemyController_Scientist : EnemyController
 
     PlayerManager _player => PlayerManager.Instance;
 
-    float _skillDelayTime = 10f;
+    float _skillDelayTime = 15f;
     float _timer = 0f;
 
     protected override void Start()
@@ -49,8 +49,31 @@ public class EnemyController_Scientist : EnemyController
 
     IEnumerator AttackSkill()
     {
-        GameObject go = InsBullet(_bombPrefab);
-        go.transform.position = _bombTransform.position;
+        // 잘못 인식된 경우 나가기
+        if (Vector3.Distance(transform.position, _player.transform.position) > _agent.stoppingDistance)
+        {
+            _isCurrentAttackCor = false;
+            ChangeState<EnemyAttackWalkState>();
+
+            yield break;
+        }
+
+        _isCurrentAttackCor = true;
+        _agent.isStopped = true;
+
+        Vector3 dir = _player.transform.position - transform.position;
+        float timer = 0;
+        while (timer <= 1f)       // 약간의 delay. temp 값.
+        {
+            timer += Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 2);
+            yield return new WaitForEndOfFrame();
+
+        }
+
+        _anim.SetTrigger("Attack");
+
+
 
         yield return null;
         _timer = 0;         // 공격 스킬이 끝났을 때
@@ -101,9 +124,7 @@ public class EnemyController_Scientist : EnemyController
         Vector3 dir = _player.transform.position - transform.position;
 
         //공격 총알 생성
-        GameObject bullet = InsBullet(_bulletPrefab
-            
-            );
+        GameObject bullet = InsBullet(_bulletPrefab);
         bullet.transform.position = _attackTransform.position;
 
         // damage 할당
@@ -111,6 +132,19 @@ public class EnemyController_Scientist : EnemyController
         eb._damage = Damage;
         eb.Owner = gameObject;
 
+    }
+
+    /// <summary>
+    /// animation clip event로 폭탄 생성
+    /// </summary>
+    public void SpawnBombEvent()
+    {
+        GameObject go = InsBullet(_bombPrefab);
+        go.transform.position = _bombTransform.position;
+
+        EnemyBomb _enemyBomb = go.GetComponent<EnemyBomb>();
+        _enemyBomb._damage = Damage;
+        _enemyBomb._owner = this.gameObject;
     }
 
     /// <summary>
