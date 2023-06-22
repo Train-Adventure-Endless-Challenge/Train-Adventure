c1c5e57c16557d47d98cb5bcddbc7b2b91f9adb2
EnSung <dmstjd0911@naver.com>
2023-06-22 21:01:31 +0900

    :sparkles: | 아이템 오브젝트 스크립트 추가

---
+++ Assets/Scripts/Item/ItemObject.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour
+{
+    public int Id { get; set; }
+
+    private ItemData _itemData;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
