0d3e9ce95b32f44f5f02a3397b4f45f2073f97d6
EnSung <dmstjd0911@naver.com>
2023-06-23 09:28:26 +0900

    :sparkles: | 아이템 초기화 추가
    - 아이템의 프리팹을 소환하고 데이터를 세팅해주는 로직 추가

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class ItemObject : MonoBehaviour
 {
-    public int Id { get; set; }
+    public int Id;
 
-    private ItemData _itemData;
+    [SerializeField] private ItemData _itemData;
+
     void Start()
     {
-        
+        GameObject itemObj =
+            Instantiate(ItemDataManager.Instance.ItemPrefab[Id] as GameObject, transform.position, Quaternion.identity);
+        itemObj.transform.parent = transform;
+
+        _itemData = ItemDataManager.Instance.ItemData[Id];
     }
 
-    void Update()
-    {
-        
-    }
 }
