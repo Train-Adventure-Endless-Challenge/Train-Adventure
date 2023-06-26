9bcaeaa165451021ab1a5b7d2cda112fd4de2990
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-26 14:26:49 +0900

    :sparkles: ㅣ 흔들림 증가 조건 변수 설정
    serialzefield로 prefab에서 설정

--- Assets/Scripts/ObjectInTrain/ObjectInTrain.cs
+++ Assets/Scripts/ObjectInTrain/ObjectInTrain.cs
@@ -10,7 +10,7 @@ public abstract class ObjectInTrain : Entity
     /// <summary>
     /// 오브젝트가 흔들림에 따른 효과를 실행 시킬 흔들림 수치
     /// </summary>
-    private int _activateShakingCondition;
+    [SerializeField] private int _activateShakingCondition;
     private bool _isActivate;
 
     public override void Die()
