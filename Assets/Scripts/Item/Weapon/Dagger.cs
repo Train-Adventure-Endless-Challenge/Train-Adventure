16a2921acbd21d53ad2e95444d8d106372da211c
EnSung <dmstjd0911@naver.com>
2023-06-28 09:53:57 +0900

    :sparkles: | 스킬 로직 작성

--- Assets/Scripts/Item/Weapon/Dagger.cs
+++ Assets/Scripts/Item/Weapon/Dagger.cs
@@ -11,10 +11,39 @@ public class Dagger : Weapon
 
     private List<GameObject> _detectionLists = new List<GameObject>();
 
+    [SerializeField] private GameObject _skillEffectPrefab;
+    [SerializeField] private float _skillRadius;
+    [SerializeField] private Transform skillImpactPosition;
     protected override void Init()
     {
         base.Init();
         _weaponCollider = GetComponent<CapsuleCollider>();
+    }
+
+    public override void UseActiveSkill()
+    {
+        base.UseActiveSkill();
+        Instantiate(_skillEffectPrefab, skillImpactPosition.position, Quaternion.identity);
+        Collider[] colliders = Physics.OverlapSphere(PlayerManager.Instance.gameObject.transform.position, _skillRadius, LayerMask.GetMask(_targetLayer));
+        Debug.Log(colliders.Length);
+
+        if (colliders.Length <= 0) return;
+
+        Shake();
+
+        foreach (Collider col in colliders)
+        {
+            Debug.Log(col.gameObject.name);
+            col.gameObject.GetComponentInParent<Entity>().Hit(_damage * 1.5f, gameObject);
+            break;
+        }
+
+    }
+
+    public override void SkillEventFunc()
+    {
+        base.SkillEventFunc();
+        UseActiveSkill();
     }
 
     public override void Attack()
@@ -41,5 +70,11 @@ public class Dagger : Weapon
             Destroy(Instantiate(_hittingFeelingEffect, collision.contacts[0].thisCollider.transform.position, collision.transform.rotation), 2);
             collision.gameObject.GetComponent<Entity>().Hit(_damage, _playerTransform.gameObject);
         }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(skillImpactPosition.position, _skillRadius);
     }
 }
