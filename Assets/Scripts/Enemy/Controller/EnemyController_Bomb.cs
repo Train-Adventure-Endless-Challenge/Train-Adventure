84df3a9cd6abace8e6ed1317a4c1ae73ff17ae6d
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-06-29 12:24:39 +0900

    :sparkles: ㅣ bomb attack logic

--- Assets/Scripts/Enemy/Controller/EnemyController_Bomb.cs
+++ Assets/Scripts/Enemy/Controller/EnemyController_Bomb.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
 
 public class EnemyController_Bomb : EnemyController
@@ -19,5 +20,25 @@ public class EnemyController_Bomb : EnemyController
     public override void Attack()
     {
 
+    }
+
+    /// <summary>
+    /// 폭탄 터짐
+    /// animation event
+    /// </summary>
+    public void AttackEvent()
+    {
+        if (Vector2.Distance(transform.position, _player.transform.position) > 5)
+            _player.GetComponent<Player>().Hit(Damage, gameObject);
+
+        // 파티클 추가
+    }
+
+    /// <summary>
+    /// 공격 실행시 죽음 
+    /// </summary>
+    public void EndAnimationEvent()
+    {
+        ChangeState<EnemyDieState>();
     }
 }
