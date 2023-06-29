65256dada80052a66d467d1b3af6f217d86ce6c2
정명직 <jmj51755245@gmail.com>
2023-06-29 11:31:08 +0900

    ✨ | StoreTrain 추가

--- Assets/Scripts/InteractionObjects/StoreItemObject.cs
+++ Assets/Scripts/InteractionObjects/StoreItemObject.cs
@@ -4,23 +4,35 @@ using UnityEngine;
 
 public class StoreItemObject : InteractionObject
 {
+    [SerializeField] private int _cost;
     [SerializeField] private ItemData _itemData;
-    [SerializeField] private int cost;
 
-
-    private void Start()
+    public void Init(ItemData itemData, int cost)
     {
-        Init();   
-    }
+        _itemData = itemData;
 
-    public void Init()
-    {
+        _cost = cost;
+
         GameObject itemObj =
-           Instantiate(ItemDataManager.Instance.ItemPrefab[_itemData.Id] as GameObject, transform.position, Quaternion.identity);
+           Instantiate(ItemDataManager.Instance.ItemPrefab[itemData.Id] as GameObject, transform.position, Quaternion.identity);
+        itemObj.transform.parent = transform;
+
+        itemObj.AddComponent<Outline>().enabled = false;
     }
 
     public override void Interact()
     {
+        if (GearManager.Instance.GearAmount < _cost)
+        {
+            Debug.Log("기어가 없습니다.");
+            // 나중에 UI로 구현
+            return;
+        }
 
+        InventoryManager.Instance.AddItem(new Item(_itemData));
+
+        GearManager.Instance.SubGear(_cost);
+
+        Destroy(gameObject);
     }
 }
