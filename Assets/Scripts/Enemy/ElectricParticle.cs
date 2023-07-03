78eca472baca214b60e1ce08f126ff6edd90f457
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-07-03 23:46:51 +0900

    :sparkles: ㅣ boss 차징 파티클 로직

---
+++ Assets/Scripts/Enemy/ElectricParticle.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Boss Enemy가 Punch 공격을 차징 중일때 활성화 되는 파티클
+/// </summary>
+public class ElectricParticle : MonoBehaviour
+{
+    ParticleSystem _particle;
+
+    private void Awake()
+    {
+        _particle = GetComponentInChildren<ParticleSystem>();
+    }
+
+    private void OnEnable()
+    {
+        _particle.Play();           // 파티클 오브젝트를 활성화 할때마다 파티클을 실행시켜줘야 한다
+    }
+}
