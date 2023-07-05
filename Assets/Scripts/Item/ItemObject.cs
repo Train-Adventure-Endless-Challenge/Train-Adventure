c19520f96c1e45465f6d5f5b320dd9e1a3027e79
EnSung <dmstjd0911@naver.com>
2023-07-05 09:03:49 +0900

    :bug: | 아이템 드롭 시 아이템 두개 생성되는ㅂ ㅓ그 수정
    - 아이템을 드롭했을 때 아이템이 두 개 생성되는 버그 수정

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -9,26 +9,35 @@ public class ItemObject : InteractionObject
     private bool _isDataReset;                           // 드랍으로 인해  생긴 오브젝트인가
     [SerializeField] private ItemData _itemData;
 
+    [SerializeField] private bool isFieldDrop;                                // 상자등으로 얻는게아닌 필드드랍인가?
     private void Start()
     {
         // 상자나 drop으로 얻은 것이아닌 그냥 바닥에 떨어져있을 경우에만 실행하기
-        Init(_item, _isDataReset);
+        if(isFieldDrop == true)
+            Init(_item, _isDataReset);
     }
 
     public void Init(Item item, bool isDataReset = false) 
     {
         GameObject itemObj =
-           Instantiate(ItemDataManager.Instance.ItemPrefab[_itemData.Id] as GameObject, transform.position, Quaternion.identity);
+            Instantiate(ItemDataManager.Instance.ItemPrefab[_itemData.Id] as GameObject, transform.position, Quaternion.identity);
         itemObj.transform.parent = transform;
+
         Item tempItem = itemObj.GetComponent<Item>();
 
-        if(isDataReset == true) // 혹시라도 드랍, 강화로 인해 아이템 데이터가 바뀐 상황이라면
+        if (isDataReset == true) // 혹시라도 드랍, 강화로 인해 아이템 데이터가 바뀐 상황이라면
             tempItem.UpdateData(item);  // 아이템 업데이트
-        
-        tempItem.enabled = false;
 
+        tempItem.enabled = false;
         itemObj.AddComponent<Outline>().enabled = false;
-        
+
+
+
+
+
+
+
+
         _isDataReset = isDataReset;
         _item = item;
         _itemData = ItemDataManager.Instance.ItemData[_itemData.Id];
