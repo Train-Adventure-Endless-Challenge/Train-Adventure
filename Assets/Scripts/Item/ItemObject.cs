e0437b54ccfa4620f68d402d145de1b75586b6d4
EnSung <dmstjd0911@naver.com>
2023-07-05 15:53:22 +0900

    :bug: | 아이템 id때문에 대검이 단검으로 스폰되던 버그 수정
    - 아이템 object엔 기본적으로 dagger가 들어가있어 itemData의 id가아닌 item의 id를 가져와 스폰하도록 수정

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -12,17 +12,24 @@ public class ItemObject : InteractionObject
     private void Start()
     {
         // 상자나 drop으로 얻은 것이아닌 그냥 바닥에 떨어져있을 경우에만 실행하기
+        _item = new Item(_itemData);
         Init(_item, _isDataReset);
     }
 
+    private void Update()
+    {
+        Object itemPrefab = ItemDataManager.Instance.ItemPrefab[_itemData.Id];
+        Item item = itemPrefab.GetComponent<Item>();
+        Debug.Log("item:        " + item.ToString());
+    }
     public void Init(Item item, bool isDataReset = false) 
     {
         GameObject itemObj =
-           Instantiate(ItemDataManager.Instance.ItemPrefab[_itemData.Id] as GameObject, transform.position, Quaternion.identity);
+           Instantiate(ItemDataManager.Instance.ItemPrefab[item.Id] as GameObject, transform.position, Quaternion.identity);
         itemObj.transform.parent = transform;
         Item tempItem = itemObj.GetComponent<Item>();
 
-        if(isDataReset == true) // 혹시라도 드랍, 강화로 인해 아이템 데이터가 바뀐 상황이라면
+        if(isDataReset == false) // 혹시라도 드랍, 강화로 인해 아이템 데이터가 바뀐 상황이라면
             tempItem.UpdateData(item);  // 아이템 업데이트
         
         tempItem.enabled = false;
@@ -31,7 +38,7 @@ public class ItemObject : InteractionObject
         
         _isDataReset = isDataReset;
         _item = item;
-        _itemData = ItemDataManager.Instance.ItemData[_itemData.Id];
+        _itemData = ItemDataManager.Instance.ItemData[item.Id];
     }
 
 
