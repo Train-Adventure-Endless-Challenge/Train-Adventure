c37af5c57496028bc9f6ec0c16263dcc902bd201
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-07-26 14:36:03 +0900

    :sparkles: ㅣ shakeMachine Gear Direction

--- Assets/Scripts/InteractionObjects/ShakeMachine.cs
+++ Assets/Scripts/InteractionObjects/ShakeMachine.cs
@@ -8,6 +8,10 @@ public class ShakeMachine : InteractionObject
     [SerializeField] private AudioClip _shakeMachineOpenSound;  // 열리는 효과음
     [SerializeField] private AudioClip _shakeMachineCloseSound; // 닫히는 효과음
 
+    [Header("Direction")]       // 연출을 위한 변수
+    [SerializeField] Transform _spawnGearTrans;
+    [SerializeField] GameObject _gearObj;       // 기어 모델 object
+
     private void Start()
     {
         _anim = GetComponent<Animator>();
@@ -19,6 +23,8 @@ public class ShakeMachine : InteractionObject
         {
             ShakeManager.Instance.ClearShake();
             GearManager.Instance.SubGear(necessaryGear);
+
+            RepairAction();
         }
         else
         {
@@ -33,6 +39,10 @@ public class ShakeMachine : InteractionObject
             _anim.SetBool("Open", true);
             SoundManager.Instance.SFXPlay(_shakeMachineOpenSound);
         }
+        else if (other.CompareTag("Gear"))
+        {
+            Destroy(other.gameObject,2f);
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -42,5 +52,17 @@ public class ShakeMachine : InteractionObject
             _anim.SetBool("Open", false);
             SoundManager.Instance.SFXPlay(_shakeMachineCloseSound);
         }
+    }
+
+    /// <summary>
+    /// 기어 수리 시 연출
+    /// </summary>
+    private void RepairAction()
+    {
+        //닫히는 연출
+        _anim.SetBool("Open", false);
+        SoundManager.Instance.SFXPlay(_shakeMachineCloseSound);
+
+        Instantiate(_gearObj,_spawnGearTrans.transform.position, Quaternion.identity);
     }
 }
