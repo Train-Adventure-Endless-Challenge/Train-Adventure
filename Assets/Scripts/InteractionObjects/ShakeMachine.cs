b4d3154e01a52927f5e42ade70b00dafa912f9f9
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-07-26 14:36:23 +0900

    :lock: ㅣ필요 기어가 0개일때 예외처리

--- Assets/Scripts/InteractionObjects/ShakeMachine.cs
+++ Assets/Scripts/InteractionObjects/ShakeMachine.cs
@@ -19,6 +19,9 @@ public class ShakeMachine : InteractionObject
     public override void Interact()
     {
         int necessaryGear = (int)ShakeManager.Instance.ShakeAmount * 5;
+
+        if (necessaryGear < 0) return;          // 기어가 0개 일땐 예외처리
+
         if (GearManager.Instance.GearAmount >= necessaryGear)
         {
             ShakeManager.Instance.ClearShake();
