98566cef942d2fc7979225b3abecbb1504efdfd6
qkrwoaks <qkrwoaks5327@naver.com>
2023-07-28 14:11:32 +0900

    🎨 | 플레이어 움직임 변경을 위해 카메라와 기차의 각도 변경
    - 카메라와 기차의 각도를 바꿈

--- Assets/Scripts/Managers/GameFlow/InGameManager.cs
+++ Assets/Scripts/Managers/GameFlow/InGameManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;   
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 
 public class InGameManager : SceneSingleton<InGameManager>
 {
@@ -12,7 +11,7 @@ public class InGameManager : SceneSingleton<InGameManager>
     [SerializeField] private GameObject[] _nomalTrainObjects;
     [SerializeField] private GameObject _bossTrainObject;
     [SerializeField] private GameObject _storeTrainObject;
-    
+
     [Header("UI")]
     [SerializeField] private Image fadeImage;  // 페이드 이미지
 
@@ -21,8 +20,10 @@ public class InGameManager : SceneSingleton<InGameManager>
     private Train _currentTrain;               // 현재 기차       
     private Train _nextTrain;                  // 다음 기차
 
-    private Vector3 _trainInterval = new Vector3(0, 0, 1.5f);   // 기차 간격
+    private Vector3 _trainInterval = new Vector3(2.25f, 0, 2.25f);   // 기차 간격
     private Vector3 _startPosition = Vector3.zero;
+
+    private Quaternion _trainRotation = Quaternion.Euler(new Vector3(0, 45, 0));
 
     private int _score;
     public int Score { get { return _score; } }
@@ -35,9 +36,10 @@ public class InGameManager : SceneSingleton<InGameManager>
 
         _nextTrain = CreateTrain(_nomalTrainObjects[Random.Range(0, _nomalTrainObjects.Length)], _startPosition);
 
-        _nextTrain.transform.position += new Vector3(0, 0, 
-            (_nextTrain._floor.transform.localScale.z / 2)
-            + (_currentTrain._floor.transform.localScale.z / 2)) + _trainInterval;
+        _nextTrain.transform.position += new Vector3((_nextTrain._floor.transform.localScale.x / 1.5f)
+            + (_currentTrain._floor.transform.localScale.x / 1.5f), 0,
+            (_nextTrain._floor.transform.localScale.z / 3)
+            + (_currentTrain._floor.transform.localScale.z / 3)) + _trainInterval;
 
         PlayerManager.Instance.gameObject.transform.position = _currentTrain._playerSpawnPoint.position;
     }
@@ -81,12 +83,13 @@ public class InGameManager : SceneSingleton<InGameManager>
         _nextTrain = CreateTrain(train, _currentTrain.transform.position);
 
         // 두 개의 기차 칸의 바닥 절반의 크기와 trainInterval 더해 nextPosition을 구함
-        Vector3 nextPosition = new Vector3(0, 0, (_nextTrain._floor.transform.localScale.z / 2)
-            + (_currentTrain._floor.transform.localScale.z / 2)) + _trainInterval;
-        
+        Vector3 nextPosition = new Vector3((_nextTrain._floor.transform.localScale.x / 1.5f)
+            + (_currentTrain._floor.transform.localScale.x / 1.5f), 0,
+            (_nextTrain._floor.transform.localScale.z / 3)
+            + (_currentTrain._floor.transform.localScale.z / 3)) + _trainInterval;
         _nextTrain.transform.position += nextPosition;
 
-        _backgroundGroup.position += new Vector3(0, 0, nextPosition.z);
+        _backgroundGroup.position += new Vector3(nextPosition.x, 0, nextPosition.z);
     }
 
     /// <summary>
@@ -95,9 +98,9 @@ public class InGameManager : SceneSingleton<InGameManager>
     /// <param name="train">생성할 기차</param>
     /// <param name="position">위치</param>
     /// <returns></returns>
-    private Train CreateTrain(GameObject train , Vector3 position)
+    private Train CreateTrain(GameObject train, Vector3 position)
     {
-        return Instantiate(train, position, Quaternion.identity).GetComponent<Train>();
+        return Instantiate(train, position, _trainRotation).GetComponent<Train>();
     }
 
     /// <summary>
