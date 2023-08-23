ca5e0e06248e034ff798a30a19755239de065000
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2023-08-23 12:05:33 +0900

    :art: ㅣ 코드리뷰 적용
    변수 생성

--- Assets/Scripts/Enemy/EnemySpawn/EnemySpawnBomb.cs
+++ Assets/Scripts/Enemy/EnemySpawn/EnemySpawnBomb.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class EnemySpawnBomb : MonoBehaviour
 {
+    [Header("Enemy")]
     float _attackDelayTime = 10f;
+    float _increaseAmount = 1f;
+ 
     public float _damage;
 
     public GameObject Owner { get; set; }
 
+    
     [SerializeField] GameObject _effectPrefab;      // 폭탄이 터지는 effect
     [SerializeField] SpriteRenderer _circleColor;   // 아래 원 서클 
+    [SerializeField] 
+
     void Start()
     {
         StartCoroutine(AttackCor());
+        _circleColor.color = Color.white;
+
+    }
+    
+    private void Update()
+    {
+        _circleColor.color = Color.Lerp(_circleColor.color, Color.red, 3f);
     }
 
     IEnumerator AttackCor()
     {
-        yield return new WaitForSeconds(_attackDelayTime - 2); // 터지기 2초 전에
+        yield return new WaitForSeconds(_attackDelayTime); 
 
-        // 터지기 전 효과
-        _circleColor.color = Color.white;
-
-        yield return new WaitForSeconds(2);      // 10초 후에
 
         // 폭탄 터짐 
         Collider[] collider = Physics.OverlapSphere(transform.position, 2f);
@@ -36,7 +45,7 @@ public class EnemySpawnBomb : MonoBehaviour
         }
 
         // 흔들림 증가
-        ShakeManager.Instance.IncreaseShake(1f);
+        ShakeManager.Instance.IncreaseShake(_increaseAmount);
 
         // 폭파되는 파티클 생성
         GameObject effect = Instantiate(_effectPrefab,transform.position,Quaternion.identity);
