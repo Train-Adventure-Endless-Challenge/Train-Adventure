15d5677ca5024b725b6198439023e3c21b3ca0ee
정명직 <jmj51755245@gmail.com>
2023-08-28 10:01:33 +0900

    :art: | 몬스터 죽었을 때 Collider 비활성화

--- Assets/Scripts/Enemy/State/EnemyDieState.cs
+++ Assets/Scripts/Enemy/State/EnemyDieState.cs
@@ -21,6 +21,8 @@ public class EnemyDieState : State<EnemyController>
         // 만약 이미 죽고 있다면 (죽는 애니메이션 실행 중이라면) return
         if (_enemyController._anim.GetCurrentAnimatorStateInfo(0).IsName("Die")) return;
 
+        _enemyController.GetComponent<CapsuleCollider>().enabled = false;   
+
         _enemyController._isDie = true;
 
         _enemyController._anim.SetBool("DieTrue",true);
