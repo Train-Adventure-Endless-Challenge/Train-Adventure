83754f8ea12ddf40e82aa822c4ba0579eced4ffb
정명직 <jmj51755245@gmail.com>
2023-08-28 10:24:31 +0900

    :art: | 변수명 변경
    - col -> cols

--- Assets/Scripts/Enemy/State/EnemyDieState.cs
+++ Assets/Scripts/Enemy/State/EnemyDieState.cs
@@ -21,9 +21,9 @@ public class EnemyDieState : State<EnemyController>
         // 만약 이미 죽고 있다면 (죽는 애니메이션 실행 중이라면) return
         if (_enemyController._anim.GetCurrentAnimatorStateInfo(0).IsName("Die")) return;
 
-        Collider[] col =  _enemyController.GetComponentsInChildren<Collider>();   
+        Collider[] cols =  _enemyController.GetComponentsInChildren<Collider>();   
         
-        foreach (Collider collider in col)
+        foreach (Collider collider in cols)
         {
             collider.enabled = false;
         }
