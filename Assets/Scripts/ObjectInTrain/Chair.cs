6f779faff75576936d41c3360873362d14a1259f
EnSung <dmstjd0911@naver.com>
2023-08-30 15:16:01 +0900

    :bug: | 몬스터가 의자를 뚫고 오는 버그 수정

--- Assets/Scripts/ObjectInTrain/Chair.cs
+++ Assets/Scripts/ObjectInTrain/Chair.cs
@@ -6,6 +6,7 @@ public class Chair : Entity
 {
     [SerializeField] List<GameObject> models= new List<GameObject>(); // 완전한 의차, 반파된 의자, 파괴된 의자.
     [SerializeField] List<Collider> colliders = new List<Collider>();
+    [SerializeField] NavMeshObstacle _navMeshObstacle;
     int brokenIdx = 0;
     protected override void Start()
     {
@@ -25,9 +26,11 @@ public class Chair : Entity
         models[brokenIdx].SetActive(true);
         if (colliders.Count > brokenIdx) colliders[brokenIdx].enabled = true;
 
+        if (_hp <= 0) Die();
     }
 
     public override void Die()
     {
+        _navMeshObstacle.enabled = false;
     }
 }
